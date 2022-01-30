5f446797338ad9ebb6919a7f8fd66347b6898950
mrOctopus001 <saefulanwaroktariansah@gmail.com>
2022-01-30 23:23:17 +0700

    Update detailGroup

--- lib/src/views/detail_group/detailGroup.dart
+++ lib/src/views/detail_group/detailGroup.dart
@@ -41,59 +41,76 @@ class BodyDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Center(
       child: Padding(
         padding: EdgeInsets.all(10),
         child: Container(
-          width: 317,
-          height: 600,
+          width: double.infinity,
+          height: double.infinity,
           decoration: BoxDecoration(
-            color: Colors.white, 
-            borderRadius: BorderRadius.all(Radius.circular(25))
-          ),child: Padding(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(25))),
+          child: Padding(
             padding: EdgeInsets.all(15),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Tim_Nganjuk',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
-                  ),
-                  ),
+                Carddetail(),
                 Padding(padding: EdgeInsets.only(bottom: 10)),
-                Text(
-                  'Participan',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold
-                  ),
-                  ),
-                  Padding(padding: EdgeInsets.only(bottom: 5)),
-                  Text(
-                    'name : ',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  Text(
-                    'Item : ', 
-                    style: TextStyle(
-                      fontSize: 16
-                    ),
-                  ),
-                  Text(
-                    'Subprice : ',
-                    style: TextStyle(
-                      fontSize: 16
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.grey,
-                  )
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Carddetail extends StatefulWidget {
+  const Carddetail({Key? key}) : super(key: key);
+
+  @override
+  _CarddetailState createState() => _CarddetailState();
+}
+
+class _CarddetailState extends State<Carddetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        color: Color(0xFF2255DC),
+        child: InkWell(
+          onTap: () {
+            print('Tapped');
+          },
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 15, top: 10),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 2, left: 5),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Group name :", 
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white
+                            ),
+                          ),
+                          
+
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
