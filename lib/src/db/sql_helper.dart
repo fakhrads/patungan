d317a4990785c9d34832772e0aa303314c0422ed
kuroyukinesha <kuroyukinesha@gmail.com>
2022-02-05 15:37:47 +0700

    Merge branch 'main' of https://github.com/fakhrads/patungan

--- lib/src/db/sql_helper.dart
+++ lib/src/db/sql_helper.dart
@@ -10,15 +10,22 @@ class SQLHelper {
         nama_grup TEXT,
         tgl_dibuat TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         catatan TEXT
+<<<<<<< HEAD
+      )
+      """);
+    await database.execute("""
+=======
       );
 
+>>>>>>> 81220651f93b6e1373fd85877c4807754260a9c3
       CREATE TABLE peserta (
         id_peserta INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         id_grup INTEGER,
         nama_peserta TEXT,
         FOREIGN KEY(id_grup) REFERENCES grup_transaksi (id_grup)
-      );
-
+      )
+      """);
+    await database.execute("""
       CREATE TABLE split_bills (
         id_split_bills INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         id_grup INTEGER,
@@ -47,20 +54,20 @@ class SQLHelper {
   }
 
   //create
-  static Future<List<int>> createGroup(
-      String nama_grup, String? catatan, String nama_peserta) async {
+  //static Future<List<int>> createGroup(String nama_grup, String? catatan, String nama_peserta) (JAN DIHAPUS)
+  static Future<int> createGroup(String nama_grup, String? catatan) async {
     final db = await SQLHelper.db();
     final dataGrup = {'nama_grup': nama_grup, 'catatan': catatan};
-    final dataPeserta = {'nama_peserta': nama_peserta};
+    // final dataPeserta = {'nama_peserta': nama_peserta};
     final idGrup = await db.insert('grup_transaksi', dataGrup,
         conflictAlgorithm: sql.ConflictAlgorithm.replace);
-    final idPeserta = await db.insert('peserta', dataPeserta,
-        conflictAlgorithm: sql.ConflictAlgorithm.replace);
-    return <int>[idGrup, idPeserta];
+    // final idPeserta = await db.insert('peserta', dataPeserta,
+    //     conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    return idGrup;
   }
 
   //read all (journal)
-  static Future<List<Map<String, dynamic>>> getItems() async {
+  static Future<List<Map<String, dynamic>>> getGrup() async {
     final db = await SQLHelper.db();
     final idGrup = db.query('grup_transaksi', orderBy: "id_grup");
     // final idPeserta = db.query('peserta', orderBy: "id_peserta");
@@ -74,17 +81,33 @@ class SQLHelper {
   // }
 
   //update
-  // static Future<int> updateItem(int id_grup, int id_peserta, String nama_grup,
-  //     String? catatan, String nama_peserta) async {
-  //   final db = await SQLHelper.db();
+  //Future<int> updateGrup(int id_grup, int id_peserta, String nama_grup,
+  //    String? catatan, String nama_peserta)
+  static Future<int> updateGrup(
+      int id_grup, String nama_grup, String? catatan) async {
+    final db = await SQLHelper.db();
 
-  //   final data = {
-  //     'nama_grup': nama_grup,
-  //     'catatan': catatan,
-  //     'nama_peserta': nama_peserta,
-  //     'tgl_dibuat': DateTime.now().toString()
-  //   };
+    final data = {
+      'nama_grup': nama_grup,
+      'catatan': catatan,
+      // 'nama_peserta': nama_peserta,
+      'tgl_dibuat': DateTime.now().toString()
+    };
 
-  //   final result = await db.update('items', data, where: "id = ?", whereArgs: [id]);
-  // }
+    final result = await db.update('grup_transaksi', data,
+        where: "id_grup = ?", whereArgs: [id_grup]);
+
+    return result;
+  }
+
+  //delete
+  static Future<void> deleteGrup(int id_grup) async {
+    final db = await SQLHelper.db();
+    try {
+      await db
+          .delete("grup_transaksi", where: "id_grup = ?", whereArgs: [id_grup]);
+    } catch (err) {
+      debugPrint("Something went wrong when deleting an item: $err");
+    }
+  }
 }
