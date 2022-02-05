da5d4f3e0451e5143f19ee0b4641dc3baa1f1c2c
mrOctopus001 <saefulanwaroktariansah@gmail.com>
2022-02-05 18:40:48 +0700

    Update editDetailGroup

--- lib/src/views/edit_group/editDetailGroup.dart
+++ lib/src/views/edit_group/editDetailGroup.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:patungan/src/components/detail_group/detailGroup.dart';
+import '../detail_group/detailGroup.dart';
 
 class EditDetailGroup extends StatelessWidget {
   const EditDetailGroup({Key? key}) : super(key: key);
@@ -6,6 +8,7 @@ class EditDetailGroup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFF6F6F6),
       body: Text("Ini Detail Group"),
     );
   }
